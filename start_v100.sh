dec2d285cafc4dda5fb2472d92b7ad08df2b7cd1
leha24rus <leha24rus@users.noreply.github.com>
2026-03-20 09:58:14 +0700

    Export CUDA environments in start_v100.sh to allow ExLlamaV3 to find CUDA locally

--- start_v100.sh
+++ start_v100.sh
@@ -2,5 +2,9 @@
 # start_v100.sh
 # Важно: флаг --no_flash_attn предотвращает падение CUDA на Tesla V100 (sm_70)
 
+export CUDA_HOME=/usr/local/cuda-12.1
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+
 echo ">>> Запуск Text-Generation-WebUI для Tesla V100..."
 python server.py --loader ExLlamav3 "$@"
