136ff8bc4698202d2ab9f771d88e0d6b19487dad
leha24rus <leha24rus@users.noreply.github.com>
2026-03-20 09:51:39 +0700

    Remove --no_flash_attn from start_v100.sh as it is deprecated

--- start_v100.sh
+++ start_v100.sh
@@ -3,4 +3,4 @@
 # Важно: флаг --no_flash_attn предотвращает падение CUDA на Tesla V100 (sm_70)
 
 echo ">>> Запуск Text-Generation-WebUI для Tesla V100..."
-python server.py --loader ExLlamav3 --no_flash_attn "$@"
+python server.py --loader ExLlamav3 "$@"
